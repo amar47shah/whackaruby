7be61561b4d02479f658fe26fe0a8d904f3bed88
Amar Shah <amar47shah@gmail.com>
2016-03-13 16:17:30 -0400

    Separate Window from driver code

---
+++ window.rb
@@ -0,0 +1,47 @@
+require 'gosu'
+require_relative './ruby'
+
+class Window < Gosu::Window
+  def initialize
+    super width, height
+    self.caption = 'Whack the Ruby!'
+  end
+
+  def lower_x
+    0
+  end
+
+  def lower_y
+    0
+  end
+
+  def upper_x
+    width
+  end
+
+  def upper_y
+    height
+  end
+
+  private
+
+  def draw
+    ruby.draw
+  end
+
+  def update
+    ruby.update
+  end
+
+  def ruby
+    @ruby ||= Ruby.new self
+  end
+
+  def height
+    600
+  end
+
+  def width
+    800
+  end
+end
