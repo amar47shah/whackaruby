c239dd42aa41f100bf818d07b7ba47ccf01d97a7
Amar Shah <amar47shah@gmail.com>
2016-03-13 11:38:50 -0400

    Draw ruby

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -4,6 +4,13 @@ class WhackARuby < Gosu::Window
   def initialize
     super 800, 600
     self.caption = 'Whack the Ruby!'
+    @image = Gosu::Image.new 'ruby.png'
+    @x, @y = 200, 200
+    @width, @height = 50, 43
+  end
+
+  def draw
+    @image.draw(@x - @width / 2, @y - @height / 2, 1)
   end
 end
 
