ce1f90c01a40e76f48c0418dc691df92050411f5
Amar Shah <amar47shah@gmail.com>
2016-03-13 13:28:58 -0400

    Hide private methods

--- ruby.rb
+++ ruby.rb
@@ -1,8 +1,6 @@
 require 'gosu'
 
 class Ruby < Gosu::Image
-  attr_writer :x, :y, :velocity_x, :velocity_y
-
   def initialize
     super 'ruby.png'
   end
@@ -25,6 +23,18 @@ class Ruby < Gosu::Image
   def y
     @y ||= 200
   end
+
+  def velocity_x
+    @velocity_x ||= 5
+  end
+
+  def velocity_y
+    @velocity_y ||= 5
+  end
+
+  private
+
+  attr_writer :x, :y, :velocity_x, :velocity_y
 
   def height
     43
@@ -50,12 +60,12 @@ class Ruby < Gosu::Image
     upper y, height
   end
 
-  def velocity_x
-    @velocity_x ||= 5
+  def lower center, dimension
+    center - dimension / 2
   end
 
-  def velocity_y
-    @velocity_y ||= 5
+  def upper center, dimension
+    center + dimension / 2
   end
 
   def x_out_of_bounds? width
@@ -68,15 +78,5 @@ class Ruby < Gosu::Image
 
   def out_of_bounds? lower, upper, upper_bound
     lower < 0 || upper > upper_bound
-  end
-
-  private
-
-  def lower center, dimension
-    center - dimension / 2
-  end
-
-  def upper center, dimension
-    center + dimension / 2
   end
 end
